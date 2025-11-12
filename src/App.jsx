bf120e09f658aa0820ed8b9c83cb77427dc96d74
fahedoudeh <fahed.oudeh@gmail.com>
2025-11-12 12:47:59 +0100

    feat(auth): add PrivateRoute and protect private pages, and hydrate token synchronously to prevent redirect on refresh; keep PrivateRoute simple

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,84 @@ import Board from "./pages/board/Board.jsx";
 import TeachersRoom from "./pages/teachersRoom/TeachersRoom.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import NotFound from "./pages/notFound/NotFound.jsx";
+import PrivateRoute from "./routes/PrivateRoute.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* Public */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        <Route path="/lessons" element={<Lessons />} />
-        <Route path="/lessons/:id" element={<LessonDetail />} />
+        {/* Private */}
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/lessons"
+          element={
+            <PrivateRoute>
+              <Lessons />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/lessons/:id"
+          element={
+            <PrivateRoute>
+              <LessonDetail />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/activities"
+          element={
+            <PrivateRoute>
+              <Activities />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/activities/:id"
+          element={
+            <PrivateRoute>
+              <ActivityDetail />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/board"
+          element={
+            <PrivateRoute>
+              <Board />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/teachers"
+          element={
+            <PrivateRoute>
+              <TeachersRoom />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
 
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/activities/:id" element={<ActivityDetail />} />
-
-        <Route path="/board" element={<Board />} />
-        <Route path="/teachers" element={<TeachersRoom />} />
-
-        <Route path="/profile" element={<Profile />} />
-
+        {/* Fallback */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
